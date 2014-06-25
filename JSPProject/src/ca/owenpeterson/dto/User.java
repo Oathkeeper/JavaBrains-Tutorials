56ac21f16be17ae22021edc930c3608fd2a69e1d
Owen Peterson <owen.a.peterson@gmail.com>
2014-06-24 19:24:15 -0500

    inital commit of new project

---
+++ JSPProject/src/ca/owenpeterson/dto/User.java
@@ -0,0 +1,22 @@
+package ca.owenpeterson.dto;
+
+public class User {
+	private String username;
+	private String userId;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
