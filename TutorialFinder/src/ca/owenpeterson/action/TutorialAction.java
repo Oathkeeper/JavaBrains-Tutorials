6b94bee57d397394e513300d02b51252c7575f27
Owen Peterson <owen.a.peterson@gmail.com>
2014-06-29 15:05:11 -0500

    TutoriaFinderService now takes a language parameter

--- TutorialFinder/src/ca/owenpeterson/action/TutorialAction.java
+++ TutorialFinder/src/ca/owenpeterson/action/TutorialAction.java
@@ -5,10 +5,12 @@ import ca.owenpeterson.service.TutorialFinderService;
 public class TutorialAction {
 	
 	private String bestTutorialSite;
+	private String language;
 	
 	public String execute() {
 		TutorialFinderService tutorialFinderService = new TutorialFinderService();
-		setBestTutorialSite(tutorialFinderService.getBestTutorialSite());
+		setBestTutorialSite(tutorialFinderService.getBestTutorialSite(getLanguage()));
+		//System.out.println(getLanguage());
 		
 		return "success";
 	}
@@ -21,5 +23,14 @@ public class TutorialAction {
 		this.bestTutorialSite = bestTutorialSite;
 	}
 
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	
 	
 }
--- TutorialFinder/src/ca/owenpeterson/service/TutorialFinderService.java
+++ TutorialFinder/src/ca/owenpeterson/service/TutorialFinderService.java
@@ -2,7 +2,12 @@ package ca.owenpeterson.service;
 
 public class TutorialFinderService {
 
-	public String getBestTutorialSite() {
-		return "Java Brains";
+	public String getBestTutorialSite(String language) {
+		if (language.equals("java")) {
+			return "Java Brains";
+		}
+		else {
+			return "Language not supported yet!";
+		}
 	}
 }
