c9acee09f33ed180edb7c0b32a97c3dc0edc7ac1
Owen Peterson <owen.a.peterson@gmail.com>
2014-08-24 16:25:20 -0500

    Added two new projects from doing tutorials.

---
+++ Testmart/src/ca/owenpeterson/ProductCatalog.java
@@ -0,0 +1,32 @@
+package ca.owenpeterson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import ca.owenpeterson.business.ProductServiceImpl;
+
+@WebService(name="TestMartCatalog", portName="TestMartCatalogPort", serviceName="TestMartCatalogService",
+			targetNamespace="http://www.testmart.com")
+public class ProductCatalog {
+	
+	ProductServiceImpl productService = new ProductServiceImpl();
+
+	@WebMethod(action="fetch_categories", operationName="fetchCategories")
+	public List<String> getProductCategories() {		
+		return productService.getProductCategories();
+	}
+	
+	@WebMethod(exclude=true)
+	public List<String> getProducts(String category) {
+		return productService.getProducts(category);
+	}
+	
+	@WebMethod(exclude=true)
+	public boolean addProduct(String category, String product) {
+		return productService.addProduct(category, product);
+	}
+	
+}
