03002836d001119b7d41c5b4b79ed48e2657a6cb
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-01 13:09:35 -0500

    Added a few JAXB annotations.

--- Testmart/src/ca/owenpeterson/ProductCatalog.java
+++ Testmart/src/ca/owenpeterson/ProductCatalog.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import ca.owenpeterson.business.ProductServiceImpl;
