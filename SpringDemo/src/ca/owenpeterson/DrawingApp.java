cfe70cbb43e8ec368db5ab973157592510edd2c7
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-07 11:18:29 -0500

    Completed tutorial 7.

--- SpringDemo/src/ca/owenpeterson/DrawingApp.java
+++ SpringDemo/src/ca/owenpeterson/DrawingApp.java
@@ -1,8 +1,8 @@
 package ca.owenpeterson;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class DrawingApp {
 
@@ -10,10 +10,12 @@ public class DrawingApp {
 		// TODO Auto-generated method stub
 
 		//Triangle triangle = new Triangle();
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		
 		triangle.draw();
+
 	}
 
 }
