d7ee629192b340cb61f01e14213e72cb05286315
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-14 15:46:52 -0500

    Completed Tutorial 15

--- SpringDemo/src/ca/owenpeterson/DrawingApp.java
+++ SpringDemo/src/ca/owenpeterson/DrawingApp.java
@@ -1,17 +1,16 @@
 package ca.owenpeterson;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//Triangle triangle = new Triangle();
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		
 		triangle.draw();
--- SpringDemo/src/ca/owenpeterson/Triangle.java
+++ SpringDemo/src/ca/owenpeterson/Triangle.java
@@ -1,17 +1,45 @@
 package ca.owenpeterson;
 
-public class Triangle {
-	
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements InitializingBean, DisposableBean, ApplicationContextAware, BeanNameAware {
+
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+	private ApplicationContext context = null;
+
+//	private List<Point> points;
 
 	public void draw() {
-		
-		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
-		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
-		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+
+		System.out.println("Point A = (" + getPointA().getX() + ", "
+				+ getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", "
+				+ getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", "
+				+ getPointC().getY() + ")");
+
+		/*
+		 * for (Point point : points) { System.out.println(point.getX() + " " +
+		 * point.getY()); }
+		 */
 	}
+
+/*	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}*/
 
 	public Point getPointA() {
 		return pointA;
@@ -36,7 +64,38 @@ public class Triangle {
 	public void setPointC(Point pointC) {
 		this.pointC = pointC;
 	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		this.context = context;
+
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		System.out.println("Bean name is: " + beanName);
+
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("InitializingBean init method called for Triangle");
+		
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Disposable Beans destroy method called for Triangle");
+		
+	}
 	
+	public void myInit() {
+		System.out.println("My init method called for Triangle");
+	}
 	
+	public void myDestroy() {
+		System.out.println("My destroy method called for Triangle");
+	}
 
 }
