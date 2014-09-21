d85100553449bdb1a5fd37a8fc84c0bc02a23c79
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-21 12:52:00 -0500

    Completed Tutorial 10 - The Autowired Annotation

---
+++ SpringDemo/src/ca/owenpeterson/Circle.java
@@ -0,0 +1,30 @@
+package ca.owenpeterson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+
+public class Circle implements Shape {
+
+	
+	private Point center;
+	
+	@Override
+	public void draw() {
+		System.out.println("Drawing circle");
+		System.out.println("Circle: Point is: (" + center.getX() + ", " + center.getY() + ")");
+	}
+
+	public Point getCenter() {
+		return center;
+	}
+
+	/*@Required*/
+	@Autowired
+	@Qualifier("circleRelated")
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+	
+	
+}
---
+++ SpringDemo/src/ca/owenpeterson/Shape.java
@@ -0,0 +1,6 @@
+package ca.owenpeterson;
+
+public interface Shape {
+
+	public void draw();
+}
