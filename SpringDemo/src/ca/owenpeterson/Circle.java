ecc277092d6a8644a2b508b467cdb6316b060fde
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-21 15:51:39 -0500

    22 - using MessageSource

--- SpringDemo/src/ca/owenpeterson/Circle.java
+++ SpringDemo/src/ca/owenpeterson/Circle.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,10 +16,14 @@ public class Circle implements Shape {
 	
 	private Point center;
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@Override
 	public void draw() {
-		System.out.println("Drawing circle");
-		System.out.println("Circle: Point is: (" + center.getX() + ", " + center.getY() + ")");
+		System.out.println(this.messageSource.getMessage("drawing.circle", null,"Default Drawing Message", null));
+		System.out.println(this.messageSource.getMessage("drawing.point", new Object[] {center.getX(), center.getY()},"Default Point Message", null));
+		System.out.println(this.messageSource.getMessage("greeting", null, "Default Greeting", null));
 	}
 
 	public Point getCenter() {
@@ -44,6 +49,16 @@ public class Circle implements Shape {
 		System.out.println("Destroy of Circle");
 		
 	}
+
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	
 	
 	
 }
