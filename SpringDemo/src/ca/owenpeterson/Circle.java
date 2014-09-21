4c70ba01f0a90dd4b2b80ff02b5ad27d3013ebee
Owen Peterson <owen.a.peterson@gmail.com>
2014-09-21 14:24:12 -0500

    21 - Component and Stereotype Annotations

--- SpringDemo/src/ca/owenpeterson/Circle.java
+++ SpringDemo/src/ca/owenpeterson/Circle.java
@@ -7,7 +7,9 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Circle implements Shape {
 
 	
