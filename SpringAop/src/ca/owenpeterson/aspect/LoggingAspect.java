cb0d017e01a849d5f69af2bba56430f2ecb43b92
Owen Peterson <owen.a.peterson@gmail.com>
2014-11-23 14:14:08 -0600

    Finished tutorial 5. Working with pointcuts and expressions in AOP.

--- SpringAop/src/ca/owenpeterson/aspect/LoggingAspect.java
+++ SpringAop/src/ca/owenpeterson/aspect/LoggingAspect.java
@@ -2,13 +2,26 @@ package ca.owenpeterson.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method called.");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second advice executed.");
+		
+	}
+	
+	//runs before a get method call with any return type.
+	// ".." matches to zero or more arguments.
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
 
 }
