2a87f6d299150dfc27595b9c302154bab47a6cd8
Owen Peterson <owen.a.peterson@gmail.com>
2014-11-23 14:28:37 -0600

    Completed tutorial 6. More pointcuts for AOP.

--- SpringAop/src/ca/owenpeterson/aspect/LoggingAspect.java
+++ SpringAop/src/ca/owenpeterson/aspect/LoggingAspect.java
@@ -8,7 +8,8 @@ import org.aspectj.lang.annotation.Pointcut;
 public class LoggingAspect {
 	
 
-	@Before("allGetters()")
+	//combines pointcuts.
+	@Before("allGetters() && allCircleMethods()")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method called.");
 	}
@@ -19,9 +20,26 @@ public class LoggingAspect {
 		
 	}
 	
+	/*//combines pointcuts.
+	@Before("allGetters() && allCircleMethods()")*/
+	
 	//runs before a get method call with any return type.
 	// ".." matches to zero or more arguments.
 	@Pointcut("execution(* get*())")
 	public void allGetters() {}
+	
+	//runs before all methods in the Circle class.
+	@Pointcut("within(ca.owenpeterson.model.Circle)")
+	public void allCircleMethods(){}
 
+	//runs before all methods in any class within the model package.
+	@Pointcut("within(ca.owenpeterson.model.*)")
+	public void allModelMethods(){}
+	
+	//takes class name or interface as arguments. Looks for methods with matching arguments.
+	@Pointcut("args()")
+	public void forArgs() {
+		
+	}
+	
 }
